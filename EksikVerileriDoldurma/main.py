ad6b3698de9d9f49bf79a9f1b23a3823504943ab
ziyasarican <87414202+ziyasarican@users.noreply.github.com>
2022-12-28 15:42:23 +0300

    Create main.py

---
+++ EksikVerileriDoldurma/main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+df = pd.read_csv("/Users/ziyasarican/Desktop/eksikveriler.csv")
+
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+
+Yas = df.iloc[:,1:4].values 
+print(Yas)
+
+# Fit öğrenme işlemini yapar. Öğrenilecek değeri veririz
+imputer = imputer.fit(Yas[:,1:4])
+
+# Fit ile öğrenip transform ile öğrendiğini uygular
+Yas[:,1:4] = imputer.transform(Yas[:,1:4])
+
+print(Yas)
